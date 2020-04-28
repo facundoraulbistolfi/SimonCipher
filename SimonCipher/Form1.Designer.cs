e65c5114a3aa149d3208ae8546629b00f099d510
facundoraulbistolfi <64446909+facundoraulbistolfi@users.noreply.github.com>
2020-04-28 14:36:13 -0300

    Add files via upload

---
+++ SimonCipher/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace SimonCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb256 = new System.Windows.Forms.RadioButton();
+            this.rb192 = new System.Windows.Forms.RadioButton();
+            this.rb128 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.btnCifrar = new System.Windows.Forms.Button();
+            this.btnDesc = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSalida = new System.Windows.Forms.TextBox();
+            this.tbEntrada = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.tbKey);
+            this.panel2.Controls.Add(this.btnCifrar);
+            this.panel2.Controls.Add(this.btnDesc);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.tbSalida);
+            this.panel2.Controls.Add(this.tbEntrada);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(448, 182);
+            this.panel2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "128 bits";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb256);
+            this.groupBox1.Controls.Add(this.rb192);
+            this.groupBox1.Controls.Add(this.rb128);
+            this.groupBox1.Location = new System.Drawing.Point(331, 94);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(106, 72);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Block/Key size";
+            // 
+            // rb256
+            // 
+            this.rb256.AutoSize = true;
+            this.rb256.Location = new System.Drawing.Point(6, 48);
+            this.rb256.Name = "rb256";
+            this.rb256.Size = new System.Drawing.Size(66, 17);
+            this.rb256.TabIndex = 9;
+            this.rb256.Text = "128/256";
+            this.rb256.UseVisualStyleBackColor = true;
+            this.rb256.Click += new System.EventHandler(this.rb128_Click);
+            // 
+            // rb192
+            // 
+            this.rb192.AutoSize = true;
+            this.rb192.Location = new System.Drawing.Point(6, 33);
+            this.rb192.Name = "rb192";
+            this.rb192.Size = new System.Drawing.Size(66, 17);
+            this.rb192.TabIndex = 8;
+            this.rb192.Text = "128/192";
+            this.rb192.UseVisualStyleBackColor = true;
+            this.rb192.Click += new System.EventHandler(this.rb128_Click);
+            // 
+            // rb128
+            // 
+            this.rb128.AutoSize = true;
+            this.rb128.Checked = true;
+            this.rb128.Location = new System.Drawing.Point(6, 18);
+            this.rb128.Name = "rb128";
+            this.rb128.Size = new System.Drawing.Size(66, 17);
+            this.rb128.TabIndex = 7;
+            this.rb128.TabStop = true;
+            this.rb128.Text = "128/128";
+            this.rb128.UseVisualStyleBackColor = true;
+            this.rb128.CheckedChanged += new System.EventHandler(this.Rb128_CheckedChanged);
+            this.rb128.Click += new System.EventHandler(this.rb128_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Key";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(3, 112);
+            this.tbKey.MaxLength = 16;
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(265, 20);
+            this.tbKey.TabIndex = 10;
+            // 
+            // btnCifrar
+            // 
+            this.btnCifrar.Location = new System.Drawing.Point(3, 139);
+            this.btnCifrar.Name = "btnCifrar";
+            this.btnCifrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCifrar.TabIndex = 1;
+            this.btnCifrar.Text = "Cifrar";
+            this.btnCifrar.UseVisualStyleBackColor = true;
+            this.btnCifrar.Click += new System.EventHandler(this.btnCifrar_Click);
+            // 
+            // btnDesc
+            // 
+            this.btnDesc.Location = new System.Drawing.Point(84, 138);
+            this.btnDesc.Name = "btnDesc";
+            this.btnDesc.Size = new System.Drawing.Size(75, 23);
+            this.btnDesc.TabIndex = 9;
+            this.btnDesc.Text = "Descifrar";
+            this.btnDesc.UseVisualStyleBackColor = true;
+            this.btnDesc.Click += new System.EventHandler(this.btnDesc_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Salida";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Entrada";
+            // 
+            // tbSalida
+            // 
+            this.tbSalida.Location = new System.Drawing.Point(3, 63);
+            this.tbSalida.Name = "tbSalida";
+            this.tbSalida.Size = new System.Drawing.Size(353, 20);
+            this.tbSalida.TabIndex = 6;
+            // 
+            // tbEntrada
+            // 
+            this.tbEntrada.Location = new System.Drawing.Point(3, 24);
+            this.tbEntrada.Name = "tbEntrada";
+            this.tbEntrada.Size = new System.Drawing.Size(353, 20);
+            this.tbEntrada.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Seleccionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(362, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Seleccionar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 206);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Simon Cipher";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.Button btnCifrar;
+        private System.Windows.Forms.Button btnDesc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton rb256;
+        private System.Windows.Forms.RadioButton rb192;
+        private System.Windows.Forms.RadioButton rb128;
+        private System.Windows.Forms.TextBox tbSalida;
+        private System.Windows.Forms.TextBox tbEntrada;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ SimonCipher/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimonCipher
+{
+    public partial class Form1 : Form
+    {
+        int blockSize;
+        int keySize;
+        Simon simonCipher;
+
+        public Form1()
+        {
+            InitializeComponent();
+            blockSize = 128;
+            keySize = 128;
+            simonCipher = new Simon(blockSize, keySize);
+        }
+
+        private void rb128_Click(object sender, EventArgs e)
+        {
+            changeBlockAndKeySize();
+        }
+
+        private void changeBlockAndKeySize()
+        {
+            /*
+            if (rb1.Checked) { blockSize = 32; keySize = 64; }
+            if (rb1.Checked) { blockSize = 48; keySize = 72; }
+            if (rb1.Checked) { blockSize = 48; keySize = 96; }
+            if (rb1.Checked) { blockSize = 64; keySize = 96; }
+            if (rb1.Checked) { blockSize = 64; keySize = 128;}
+            if (rb1.Checked) { blockSize = 96; keySize = 96; }
+            if (rb1.Checked) { blockSize = 96; keySize = 144; }
+            */
+            if (rb128.Checked) { blockSize = 128; keySize = 128; tbKey.Text = ""; tbKey.MaxLength = 16; }
+            if (rb192.Checked) { blockSize = 128; keySize = 192; tbKey.Text = ""; tbKey.MaxLength = 24; }
+            if (rb256.Checked) { blockSize = 128; keySize = 256; tbKey.Text = ""; tbKey.MaxLength = 32; }
+            label4.Text = keySize + " bits";
+            simonCipher = new Simon(blockSize, keySize);
+        }
+
+        private void btnCifrar_Click(object sender, EventArgs e)
+        {
+            //var watch = System.Diagnostics.Stopwatch.StartNew();
+            FileStream inFs = new FileStream(tbEntrada.Text, FileMode.Open, FileAccess.Read);
+            FileStream outFs = new FileStream(tbSalida.Text, FileMode.Create);
+            byte[] bufferIn = new byte[16];
+            int fileOffset = 0;
+            ulong i = 0;
+
+            simonCipher.calcularKeys(Encoding.ASCII.GetBytes(tbKey.Text));
+
+            while (fileOffset < inFs.Length)
+            {
+                inFs.Seek(fileOffset, SeekOrigin.Begin);
+                bufferIn = new byte[16];
+                int bytesRead = inFs.Read(bufferIn, 0, 16);
+
+                outFs.Write(simonCipher.cifrarBloque(bufferIn), 0, 16);
+                fileOffset += 16;
+                i++;
+            }
+            
+            inFs.Close();
+            outFs.Close();
+            //watch.Stop();
+            //tbTime.Text = watch.ElapsedMilliseconds.ToString();
+            
+
+        }
+
+        private void btnDesc_Click(object sender, EventArgs e)
+        {
+            //var watch = System.Diagnostics.Stopwatch.StartNew();
+            FileStream inFs = new FileStream(tbEntrada.Text, FileMode.Open, FileAccess.Read);
+            FileStream outFs = new FileStream(tbSalida.Text, FileMode.Create);
+            byte[] bufferIn = new byte[16];
+            int fileOffset = 0;
+            ulong i = 0;
+
+            simonCipher.calcularKeys(Encoding.ASCII.GetBytes(tbKey.Text));
+
+            while (fileOffset < inFs.Length)
+            {
+                inFs.Seek(fileOffset, SeekOrigin.Begin);
+                bufferIn = new byte[16];
+                int bytesRead = inFs.Read(bufferIn, 0, 16);
+
+                
+
+                outFs.Write(simonCipher.descifrarBloque(bufferIn), 0, 16);
+                fileOffset += 16;
+                i++;
+            }
+
+            inFs.Close();
+            outFs.Close();
+            //watch.Stop();
+            //tbTime.Text = watch.ElapsedMilliseconds.ToString();
+
+        }
+
+
+        //Funcion que convierte un array de ulong a un string (En ASCII)
+        public String ulongToString(ulong[] array)
+        {
+            String decoded = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                decoded += Encoding.Unicode.GetString(BitConverter.GetBytes(array[i]));
+            }
+            return decoded;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog fd = new OpenFileDialog())
+            {
+                fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                fd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                fd.FilterIndex = 2;
+                fd.RestoreDirectory = true;
+
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    tbEntrada.Text = fd.FileName;
+                }
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog fd = new SaveFileDialog())
+            {
+                fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                fd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                fd.FilterIndex = 2;
+                fd.RestoreDirectory = true;
+
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    tbSalida.Text = fd.FileName;
+                }
+            }
+        }
+
+        private void Rb128_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+   
+}
